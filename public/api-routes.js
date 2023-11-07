7e42a868acd42f4c12f7ae23506fc205da90154b
Joshua <Joshua.soto@live.com>
2023-11-07 17:03:39 -0600

    added server.js

--- public/api-routes.js
+++ public/api-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
-const fs = require('fs');
+const fs = require ('fs');
 
 router.get('/api-notes', async (req, res ) => {
     const dbJson = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
