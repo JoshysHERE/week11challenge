347c91a66722b7c61de2e88093e076ce2b043c36
Joshua <Joshua.soto@live.com>
2023-11-06 18:11:31 -0600

    added api route

---
+++ public/api-routes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+
+router.get('/api-notes', async (req, res ) => {
+    const dbJson = await JSON.parse(fs.readFileSync("db/db.json","utf8"));
+    res.json(dbJson);
+});
+
+router.post('/api/notes', (req, res) => (
+    const dbJson = JSON.parse(fs.readFileSync("db/db.json","utf8"));
+    const newFeedback = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+    };
+));
